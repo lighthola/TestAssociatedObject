c2445dc6df8e0bc23d3683264db120e7eaff32c1
Bevis <bevis_chen@soft-world.com.tw>
2016-07-26 09:46:44 +0800

    Initial Commit

---
+++ TestAssociatedObject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAssociatedObject
+//
+//  Created by Bevis Chen on 7/26/16.
+//  Copyright © 2016 Bevis Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAssociatedObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAssociatedObject
+//
+//  Created by Bevis Chen on 7/26/16.
+//  Copyright © 2016 Bevis Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
